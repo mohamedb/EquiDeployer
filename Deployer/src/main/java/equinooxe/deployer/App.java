9cc3fc46a51169f58532179d3d2604d346bdbfa5
mohamed boullouz <mohamed.boullouz@inova-software.com>
2017-03-02 09:05:35 +0100

    init

---
+++ Deployer/src/main/java/equinooxe/deployer/App.java
@@ -0,0 +1,13 @@
+package equinooxe.deployer;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
